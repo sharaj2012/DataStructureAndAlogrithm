2aab22fcbdfd8f07e6dd4b095d3b1b82e06d7748
sharaj2012 <shashy.raj2012@gmail.com>
2020-08-05 22:39:50 +0530

    added files

---
+++ DataStructureAndAlgo/src/APlusB.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class APlusB.
+ * the class performs basic addition
+ */
+public class APlusB {
+
+	/**
+	 * Sum of two digits.
+	 *
+	 * @param firstNum the first num
+	 * @param secondNum the second num
+	 * @return the int
+	 */
+	static int sumOfTwoDigits(int firstNum, int secondNum)
+	{
+		return firstNum + secondNum ;
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+
+		System.out.println(sumOfTwoDigits(a, b));
+	}
+}
