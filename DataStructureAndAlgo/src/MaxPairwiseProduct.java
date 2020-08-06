41605f219fe6952bf43e9ce2149631275a0ae034
sharaj2012 <shashy.raj2012@gmail.com>
2020-08-06 20:46:58 +0530

    all test case passed

--- DataStructureAndAlgo/src/MaxPairwiseProduct.java
+++ DataStructureAndAlgo/src/MaxPairwiseProduct.java
@@ -1,10 +1,10 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class MaxPairwiseProduct {
-
-	private static int maxProduct(int[] numArray) {
-
-		int product = 0;
+	
+	private static long maxProductSlow(long[] numArray) {
+		long product = 0;
 		for(int i=0;i<numArray.length;i++)
 		{
 			for(int j=i+1;j<numArray.length;j++)
@@ -12,20 +12,48 @@ public class MaxPairwiseProduct {
 				product = Math.max(product, numArray[i]*numArray[j]);
 			}
 		}
-		return product;
+		return product; 
 	}
 
+	private static long maxProductFast(long[] numArray) {
+		long product = 0;
+		/*	for(int i=0;i<numArray.length;i++)
+		{
+			for(int j=i+1;j<numArray.length;j++)
+			{
+				product = Math.max(product, numArray[i]*numArray[j]);
+			}
+		} */
+		int max_index1=-1;
+		for(int i=0;i<numArray.length;i++)
+		{
+			if((max_index1 == -1)||(numArray[max_index1]<numArray[i])){
+				max_index1 = i;
+			}	
+		}
+
+		int max_index2 =-1;
+		for(int j=0;j<numArray.length;j++){
+
+			if(( j != max_index1) && ((max_index2 == -1)||(numArray[max_index2]<numArray[j]))){
+				max_index2 = j;
+			}		
+		}
+		product = numArray[max_index1]*numArray[max_index2];
+		
+		return product;
+	}
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int[] numArray = new int[n] ;
+		long[] numArray = new long[n] ;
 		for(int x=0; x<n; x++)
 		{
-			int num = scan.nextInt();
+			long num = scan.nextInt();
 			numArray[x] = num ;
 		}
-		System.out.println(maxProduct(numArray));
+		System.out.println(maxProductFast(numArray));
 		scan.close();
 	}
 }
